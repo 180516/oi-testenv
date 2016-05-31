4b7dc1d5c499fb8374b75879487bf57d0436af5c
Robert <180516@edu.p.lodz.pl>
2016-05-31 20:45:09 +0200

    merged quality evaluation

--- generate-jars.sh
+++ generate-jars.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# oi-solver
 mvn install -DskipTests -f $DIR/oi-solver/pom.xml
 cp $DIR/oi-solver/target/solver*.jar $DIR/environment/katalog/solver
+
+# quality-evaluation
+mvn install -DskipTests -f $DIR/QualityEvaluation/pom.xml
+cp $DIR/QualityEvaluation/target/quality-evaluation*.jar $DIR/environment/katalog/quality-evaluation
+
